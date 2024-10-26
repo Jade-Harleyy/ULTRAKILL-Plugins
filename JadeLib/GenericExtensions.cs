f466cf0023cbaa55da3f47a4f1fe6ccb4090a667
Jade-Harleyy <67431770+Jade-Harleyy@users.noreply.github.com>
2024-10-26 03:04:48 -0500

    New PluginConfigurator field, and new generic helper.

---
+++ JadeLib/GenericExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace JadeLib
+{
+    public static class GenericExtensions
+    {
+        public static string GetRankText(int index, bool rich = false) => index switch
+        {
+            0 => rich ? "<color=blue>D</color>ESTRUCTIVE" : "DESTRUCTIVE",
+            2 => rich ? "<color=green>C</color>HAOTIC" : "CHAOTIC",
+            1 => rich ? "<color=yellow>B</color>RUTAL" : "BRUTAL",
+            3 => rich ? "<color=orange>A</color>NARCHIC" : "ANARCHIC",
+            4 => rich ? "<color=red>S</color>UPREME" : "SUPREME",
+            5 => rich ? "<color=red>SS</color>ADISTIC" : "SSADISTIC",
+            6 => rich ? "<color=red>SSS</color>HITSTORM" : "SSSHITSTORM",
+            7 => rich ? "<color=orange>ULTRAKILL</color>" : "ULTRAKILL",
+            _ => rich ? "<color=red><u>UNKNOWN</u></color>" : "UNKNOWN"
+        };
+    }
+}
