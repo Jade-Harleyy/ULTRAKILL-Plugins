2f66620619b4c0126ef51935570ab90cd6fbe3d2
Jade-Harleyy <67431770+Jade-Harleyy@users.noreply.github.com>
2024-11-09 16:05:21 -0600

    Commissioned by Mothlematic. Changes the position of the ULTRAPAIN button in the difficulty select menu.

---
+++ UltrapainFix/Patches.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace UltrapainMenuFix
+{
+    [HarmonyPatch]
+    internal static class Patches
+    {
+        [HarmonyPatch(typeof(Ultrapain.Plugin), nameof(Ultrapain.Plugin.OnSceneChange)), HarmonyPostfix]
+        private static void Plugin_OnSceneChange()
+        {
+            // Can't use null conditional here (Unity sucks).
+            if (Ultrapain.Plugin.currentDifficultyButton == null || Ultrapain.Plugin.currentDifficultyButton.transform is not RectTransform buttonTransform) { return; }
+            buttonTransform.anchoredPosition = new Vector2(20f, -200f);
+            Transform parentTransform = buttonTransform.parent;
+            for (int i = 7; i <= 13; i++)
+            {
+                parentTransform.GetChild(i).localPosition += new Vector3(0f, i >= 11 ? 52f : 26f, 0f);
+            }
+        }
+    }
+}
---
+++ UltrapainFix/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace UltrapainMenuFix
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("com.eternalUnion.ultraPain", BepInDependency.DependencyFlags.HardDependency)]
+    internal class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            new Harmony(PluginInfo.PLUGIN_GUID).PatchAll();
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
